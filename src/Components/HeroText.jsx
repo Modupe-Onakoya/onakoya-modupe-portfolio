ace84373a8e245b0ad4e5e49ea3af1b6518beb8c
Modupe-Onakoya <onakoyamodupe0@gmail.com>
2026-07-26 19:04:10 +0100

    made update on d hero animatio

--- src/Components/HeroText.jsx
+++ src/Components/HeroText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion, AnimatePresence } from "motion/react";
 import { useEffect, useState } from "react";
 
-const phrases = ["I'm Onakoya Modupe", "A Frontend Developer"];
+const phrases = ["I'm Onakoya Modupe", "A  Frontend Developer"];
 const directions = [1, -1];
 
 const HeroText = () => {
@@ -28,7 +28,9 @@ const HeroText = () => {
                     transition={{ duration: 0.5, ease: "easeInOut" }}
                     className="text-[clamp(36px,7vw,92px)]  leading-none pb-5 max-sm:text-[24px] text-center   "
                 >
-                    {phrases[index]
+                    {phrases[index] === "I'm Onakoya Modupe"
+                        ? <>I'm Onakoya <br className="block sm:hidden" /> Modupe</>
+                        : <>A Frontend <br className="block sm:hidden" /> Developer</>
                     }
                 </motion.h1>
             </AnimatePresence>
